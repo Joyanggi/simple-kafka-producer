0a15a714a60584904716b06f6dfb4dccc855d6c2
Joyanggi <jyg8033@gmail.com>
2023-06-01 02:11:25 +0900

    Fix: ProducerRecord에 파티션을 직접 지정하여 데이터를 전송하는 경우 추가
    1. ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, partitionNo, messagekey, messageValue); 추가

--- src/main/java/SimpleProducer.java
+++ src/main/java/SimpleProducer.java
@@ -29,7 +29,10 @@ public class SimpleProducer {
 //        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue); //7. 카프카 브로커로 데이터를 보내기 위해 ProducerRecord를 생성한다. ProducerRecord는 생성자를 여러 개 가지는데, 생성자 개수에 따라 오버로딩되어 생성된다.
                                                                                                 //여기서는 토픽 이름과 메세지 값만 선언하였다. 메세지 키는 따로 선언하지 않았으므로 null로 설정되어 전송된다.
                                                                                                 //ProducerRecord를 생성할 때 생성자에 2개의 제네릭 값이 들어가는데, 이 값은 메세지 키와 메세지 값의 타입을 뜻한다. 메세지 키와 메세지 값의 타입은 직렬화 클래스와 동일하게 설정한다.
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "log4j3", "test3");
+        int partitionNo = 0;
+        String messagekey = "key";
+//        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "log4j3", "test3"); //메세지 키를 가진 데이터를 전송
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, partitionNo, messagekey, messageValue); //파티션을 직접 지정하여 데이터를 전송. 토픽이름, 파티션 번호, 메세지 키, 메세지 값을 순서대로 파라미터에 넣고 생성해야한다. 파티션번호는 토픽에 존재하는 번호로 설정해야한다.
         producer.send(record); //8. 생성한 ProducerRecord를 전송하기 위해 record를 파라미터로 가지는 send() 메서드를 호출했다. 프로듀서에서 send()는 즉각적인 전송을 뜻하는 것이 아니라, 파라미터로 들어간 record를 프로듀서 내부에 가지고 있다가 배치 형태로 묶어서 브로커에 전송한다.
                                //이러한 전송 방식을 '배치 전송'이라고 부른다. 배치 전송을 통해 카프카는 ㅌ아 메세지 플랫폼과 차별화된 전송 속도를 가지게 되었다.
         logger.info("{}", record);
